0740fcbe8ad7100a88514dead76400b2e1dc3ef9
Diedrich <kamil.diedrich@intel.com>
2017-12-12 15:03:20 +0100

    Add tests for pmempool_create

---
+++ src/utils/api_c/linux_api.cc
@@ -0,0 +1,163 @@
+/*
+ * Copyright 2017, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifdef __linux__
+
+#include <errno.h>
+#include <fcntl.h>
+#include <fts.h>
+#include <libgen.h>
+#include <sys/statvfs.h>
+#include <unistd.h>
+#include "api_c.h"
+
+int ApiC::AllocateFileSpace(const std::string &path, size_t length) {
+  if (static_cast<off_t>(length) < 0) {
+    std::cerr << "length should be > 0" << std::endl;
+    return -1;
+  }
+
+  int fd = open(path.c_str(), O_CREAT | O_RDWR);
+
+  if (fd == -1) {
+    std::cerr << "Unable to create file: " << strerror(errno) << std::endl;
+    return -1;
+  }
+
+  int ret = posix_fallocate(fd, 0, static_cast<off_t>(length));
+
+  if (ret != 0) {
+    std::cerr << "Unable to allocate disk space: " << strerror(errno)
+              << std::endl;
+    close(fd);
+    RemoveFile(path);
+  }
+
+  return ret;
+}
+
+int ApiC::GetExecutablePath(std::string &path) {
+  char file_path[FILENAME_MAX + 1] = {0};
+  ssize_t count = readlink("/proc/self/exe", file_path, FILENAME_MAX);
+
+  if (count == -1) {
+    return -1;
+  }
+
+  file_path[count] = '\0';
+  path = std::string(dirname(file_path)) + "/";
+
+  return 0;
+}
+
+long long ApiC::GetFreeSpaceT(const std::string &dir) {
+  struct statvfs fs;
+  if (statvfs(dir.c_str(), &fs) != 0) {
+    std::cerr << "Unable to get file system statistics: " << strerror(errno)
+              << std::endl;
+    return -1;
+  }
+
+  return fs.f_bsize * fs.f_bavail;
+}
+
+int ApiC::CreateDirectoryT(const std::string &dir) {
+  if (mkdir(dir.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) == -1) {
+    std::cerr << "mkdir failed: " << strerror(errno) << std::endl;
+    return -1;
+  }
+
+  return 0;
+}
+
+int ApiC::CleanDirectory(const std::string &dir) {
+  FTS *fts;
+  FTSENT *f_sent;
+  int options = FTS_COMFOLLOW | FTS_LOGICAL | FTS_NOCHDIR;
+  int ret = 0;
+  char *d[] = {(char *)dir.c_str(), nullptr};
+
+  fts = fts_open(d, options, nullptr);
+
+  if (fts == nullptr) {
+    std::cerr << "fts_open failed: " << strerror(errno) << std::endl;
+    return -1;
+  }
+
+  f_sent = fts_children(fts, 0);
+
+  if (f_sent == nullptr) {
+    return 0;
+  }
+
+  while ((f_sent = fts_read(fts)) != nullptr) {
+    switch (f_sent->fts_info) {
+      case FTS_D:
+        if (f_sent->fts_path != dir &&
+            RemoveDirectoryT(f_sent->fts_path) != 0) {
+          std::cerr << "Unable to remove directory " << f_sent->fts_path << ": "
+                    << strerror(errno) << std::endl;
+          ret = -1;
+        }
+        break;
+      case FTS_F:
+        if (RemoveFile(f_sent->fts_path) != 0) {
+          std::cerr << "Unable to remove file: " << f_sent->fts_path
+                    << std::endl;
+          ret = -1;
+        }
+        break;
+      default:
+        break;
+    }
+  }
+
+  if (fts_close(fts) != 0) {
+    std::cerr << "fts_close failed: " << strerror(errno) << std::endl;
+    ret = -1;
+  }
+
+  return ret;
+}
+
+int ApiC::RemoveDirectoryT(const std::string &dir) {
+  int ret = rmdir(dir.c_str());
+
+  if (ret != 0) {
+    std::cerr << "Unable to remove directory: " << strerror(errno) << std::endl;
+    return ret;
+  }
+
+  return 0;
+}
+
+#endif  // __linux__
---
+++ src/utils/api_c/windows_api.cc
@@ -0,0 +1,199 @@
+/*
+ * Copyright 2017, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifdef _WIN32
+
+#include <codecvt>
+#include <locale>
+#include "api_c.h"
+
+int ApiC::AllocateFileSpace(const std::string &path, size_t length) {
+  if (length < 0) {
+    std::cerr << "length should be > 0" << std::endl;
+    return -1;
+  }
+
+  HANDLE h = CreateFile(path.c_str(), GENERIC_READ | GENERIC_WRITE,
+                        FILE_SHARE_READ | FILE_SHARE_WRITE, nullptr, CREATE_NEW,
+                        FILE_ATTRIBUTE_NORMAL, nullptr);
+
+  if (h == INVALID_HANDLE_VALUE) {
+    std::cerr << "INVALID_HANDLE_VALUE occurs\nError message: "
+              << GetLastError() << std::endl;
+    return -1;
+  }
+
+  /* check if sparse files are supported */
+  DWORD flags = 0;
+  BOOL ret = GetVolumeInformationByHandleW(h, nullptr, 0, nullptr, nullptr,
+                                           &flags, nullptr, 0);
+  if (!ret) {
+    std::cerr << "GetVolumeInformationByHandleW: ";
+    goto err;
+  } else if ((flags & FILE_SUPPORTS_SPARSE_FILES) == 0) {
+    std::cerr << "Filesystem does not support sparse files" << std::endl;
+    goto err;
+  }
+
+  /* mark file as sparse */
+  DWORD nbytes;
+  ret = DeviceIoControl(h, FSCTL_SET_SPARSE, nullptr, 0, nullptr, 0, &nbytes,
+                        nullptr);
+  if (!ret) {
+    std::cerr << "DeviceIoControl: ";
+    goto err;
+  }
+
+  /* set file length */
+  LARGE_INTEGER llen;
+  llen.QuadPart = length;
+
+  DWORD ptr = SetFilePointerEx(h, llen, nullptr, FILE_BEGIN);
+  if (ptr == INVALID_SET_FILE_POINTER) {
+    std::cerr << "SetFilePointerEx: ";
+    goto err;
+  }
+
+  ret = SetEndOfFile(h);
+  if (!ret) {
+    std::cerr << "SetEndOfFile: ";
+    goto err;
+  }
+
+  return 0;
+
+err:
+  std::cerr << GetLastError() << std::endl;
+  CloseHandle(h);
+  std::remove(path.c_str());
+  return -1;
+}
+
+int ApiC::GetExecutablePath(std::string &path) {
+  char file_path[MAX_PATH + 1] = {0};
+  auto count = GetModuleFileName(nullptr, file_path, MAX_PATH);
+
+  if (count == 0 || count == MAX_PATH) {
+    return -1;
+  }
+
+  file_path[count] = '\0';
+
+  char dir[MAX_PATH + 1] = {0};
+  _splitpath(file_path, nullptr, dir, nullptr, nullptr);
+  path = std::string{dir} + "\\";
+
+  return 0;
+}
+
+long long ApiC::GetFreeSpaceT(const std::string &dir) {
+  __int64 free_bytes_available, total_number_of_bytes,
+      total_number_of_free_bytes;
+  LPCSTR drive = dir.c_str();
+  int result =
+      GetDiskFreeSpaceExA(drive, (PULARGE_INTEGER)&free_bytes_available,
+                          (PULARGE_INTEGER)&total_number_of_bytes,
+                          (PULARGE_INTEGER)&total_number_of_free_bytes);
+
+  if (result == 0) {
+    std::cerr << "Unable to get file system statistics: " << GetLastError()
+              << std::endl;
+    return -1;
+  }
+
+  return total_number_of_free_bytes;
+}
+
+int ApiC::CreateDirectoryT(const std::string &dir) {
+  BOOL ret = CreateDirectory(dir.c_str(), nullptr);
+
+  if (ret) {
+    return 0;
+  }
+
+  std::cerr << "Unable to create directory: " << GetLastError() << std::endl;
+  return -1;
+}
+
+int ApiC::CleanDirectory(const std::string &dir) {
+  int ret = 0;
+  HANDLE h = nullptr;
+  WIN32_FIND_DATA f_d;
+
+  h = FindFirstFile((dir + "*").c_str(), &f_d);
+
+  if (h == INVALID_HANDLE_VALUE) {
+    std::cerr << "INVALID_HANDLE_VALUE occurs\nError message: "
+              << GetLastError() << std::endl;
+    return -1;
+  }
+
+  do {
+    const std::string file_name = f_d.cFileName;
+    const std::string full_file_name = dir + file_name;
+    if (!lstrcmp(f_d.cFileName, ".") || !lstrcmp(f_d.cFileName, "..")) {
+      continue;
+    }
+
+    SetFilePermission(dir + f_d.cFileName, 0600);
+
+    if (DirectoryExists(dir + f_d.cFileName) &&
+        !RemoveDirectory((dir + f_d.cFileName).c_str())) {
+      std::cerr << "Unable to remove directory: " << GetLastError()
+                << std::endl;
+      ret = -1;
+    } else if (RemoveFile(dir + f_d.cFileName) != 0) {
+      std::cerr << "File " << f_d.cFileName << " removing failed" << std::endl;
+      ret = -1;
+    }
+  } while (FindNextFile(h, &f_d));
+
+  if (h != INVALID_HANDLE_VALUE && !FindClose(h)) {
+    std::cerr << "Closing dir failed: " << GetLastError() << std::endl;
+    return -1;
+  }
+
+  return ret;
+}
+
+int ApiC::RemoveDirectoryT(const std::string &dir) {
+  BOOL ret = RemoveDirectory(dir.c_str());
+
+  if (!ret) {
+    std::cerr << "Unable to remove directory: " << GetLastError() << std::endl;
+    return -1;
+  }
+
+  return 0;
+}
+
+#endif  // !_WIN32
