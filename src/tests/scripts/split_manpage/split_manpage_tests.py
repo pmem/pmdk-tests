35ed19627d66ed5b76d09cd95e448925ef570d20
Grzegorz Brzezinski <grzegorz.brzezinski@intel.com>
2020-01-22 12:56:04 +0100

    test: fix doc tests
    PMDK's process of generating documentation has changed,
    they are now stored in catalogs in 'doc' directory

--- src/tests/scripts/split_manpage/split_manpage_tests.py
+++ src/tests/scripts/split_manpage/split_manpage_tests.py
@@ -1,6 +1,6 @@
 #!usr/bin/env python3
 #
-# Copyright 2018, Intel Corporation
+# Copyright 2018-2020, Intel Corporation
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -35,30 +35,33 @@ This module includes functions to check if documentation of PMDK library
 is built correctly and tests which use these functions.
 
 Tests check:
--if all macros from header files are included in the generated directory,
--if all functions from .so files are included in the generated directory,
--if all functions and macros from the generated directory, are included
+-if all macros from header files are included in the doc directory,
+-if all functions from .so files are included in the doc directory,
+-if all functions and macros from the doc directory, are included
  in headers files and .so files,
 -if all functions and macros have direct access to man page through use
  command 'man function_name/macro_name'.
 
 Required argument:
   -r <PMDK_path>    the PMDK library root path.
+  -i <PMDK_install_path>    PMDK library installation path.
 
 Optional argument:
-  -d <PMDK_doc_path>    PMDK library documentation path. Needs to be specified
-                        if it is not in PATH.
+  --with-pmem2    the flag if libpmem2 is built.
 """
 
 import unittest
 from subprocess import check_output, call, DEVNULL
-from os import listdir, path, linesep
+from os import listdir, path, linesep, walk
 import re
 import sys
 
 # definitions of regexes
 EXPRESSION_AT_THE_LINE_START = r'[\s]*([a-zA-Z_]+)[\\(\s]+'
 EXPRESSION_AFTER_DEFINE_PHRASE = r'[\s]*#define[\s]*([a-zA-Z_]+)[\\(\s]+'
+
+PMDK_LIBRARIES = ['libpmem', 'libpmemblk', 'libpmemlog', 'libpmemobj',
+                  'libpmempool', 'librpmem']
 
 
 def get_exceptions(pmdk_path):
@@ -108,16 +111,14 @@ def get_macros(pmdk_path):
     return macros
 
 
-def get_functions_from_so_files(pmdk_path):
+def get_functions_from_so_files(lib_path):
     """
     Returns names of functions in a list based on symbols 'T' from .so files.
     """
     functions_from_so_files = []
-    path_to_so_files = path.join(pmdk_path, 'src', 'nondebug')
+    path_to_so_files = lib_path
     for elem in listdir(path_to_so_files):
-        # Exclude libvmmalloc.so, because does not include names of functions
-        # of PMDK library.
-        if elem.endswith('.so') and elem != 'libvmmalloc.so':
+        if elem.endswith('.so') and elem.split('.')[0] in PMDK_LIBRARIES:
             process = check_output(
                 'nm ' + elem + ' | grep " T "', cwd=path_to_so_files,
                 shell=True)
@@ -133,73 +134,75 @@ def get_functions_from_so_files(pmdk_path):
     return functions_from_so_files
 
 
-def get_functions_and_macros_from_generated(pmdk_path):
+def get_functions_and_macros_from_doc(pmdk_path):
     """
     Returns names of functions and macros in a list based on names of files
-    from the generated directory.
+    from the doc directory.
     """
-    path_to_functions_and_macros = path.join(pmdk_path, 'doc', 'generated')
-    # Files with extension '.3' have the same name as functions and macros
-    # of PMDK library. 'pmemobj_action' is excluded, because it is not a name
-    # of the function.
-    functions_and_macros_from_generated = [elem.split('.')[0]
-            for elem in listdir(path_to_functions_and_macros)
-            if elem.endswith('.3') and not elem.startswith('pmemobj_action')]
-    return functions_and_macros_from_generated
+    path_to_functions_and_macros = path.join(pmdk_path, 'doc')
+    functions_and_macros_from_doc = []
+    for _, _, files in walk(path_to_functions_and_macros):
+        for f in files:
+            if not f.endswith('.3'):
+                continue
+            # Files with extension '.3' have the same name as functions and
+            # macros of PMDK library. 'pmemobj_action' is excluded, because
+            # it is not a name of the function.
+            if f.startswith('pmemobj_action'):
+                continue
+            if not 'libpmem2' in PMDK_LIBRARIES and f.startswith('pmem2'):
+                continue
+            functions_and_macros_from_doc.append(f.split('.')[0])
+    return functions_and_macros_from_doc
 
 
-def check_completeness_of_macros_in_generated(pmdk_path):
+def check_completeness_of_macros_in_doc(pmdk_path):
     """
-    Returns missing macros in the generated directory in a list, based on macros
+    Returns missing macros in the doc directory in a list, based on macros
     extracted from header files.
     """
     macros = get_macros(pmdk_path)
-    functions_from_generated = get_functions_and_macros_from_generated(
-        pmdk_path)
+    functions_from_doc = get_functions_and_macros_from_doc(pmdk_path)
     missing_macros = [macro for macro in macros if macro not in
-                      functions_from_generated]
+                      functions_from_doc]
     return missing_macros
 
 
-def check_completeness_of_functions_in_generated(pmdk_path):
+def check_completeness_of_functions_in_doc(pmdk_path, lib_path):
     """
-    Returns list of missing macros in the generated directory, based on macros
+    Returns list of missing macros in the doc directory, based on macros
     extracted from .so files.
     """
-    functions_from_so_files = get_functions_from_so_files(pmdk_path)
-    functions_from_generated =\
-        get_functions_and_macros_from_generated(pmdk_path)
-    missing_functions_in_generated =\
-        [function for function in functions_from_so_files
-         if function not in functions_from_generated]
-    return missing_functions_in_generated
+    functions_from_so_files = get_functions_from_so_files(lib_path)
+    functions_from_doc = get_functions_and_macros_from_doc(pmdk_path)
+    missing_functions_in_doc = [function for function in functions_from_so_files
+                                if function not in functions_from_doc]
+    return missing_functions_in_doc
 
 
-def check_completeness_of_extracted_functions_and_macros(pmdk_path):
+def check_completeness_of_extracted_functions_and_macros(pmdk_path, lib_path):
     """
     Returns list of missing macros in header files and missing functions in .so
-    files, based on functions and macros from the generated directory.
+    files, based on functions and macros from the doc directory.
     """
-    functions_from_generated =\
-        get_functions_and_macros_from_generated(pmdk_path)
+    functions_from_doc = get_functions_and_macros_from_doc(pmdk_path)
     macros = get_macros(pmdk_path)
-    functions_from_so_files = get_functions_from_so_files(pmdk_path)
-    missing_functions_and_macros_in_generated = [
-        item for item in functions_from_generated
+    functions_from_so_files = get_functions_from_so_files(lib_path)
+    missing_functions_and_macros_in_doc = [
+        item for item in functions_from_doc
         if not (item in macros or item in functions_from_so_files)]
-    return missing_functions_and_macros_in_generated
+    return missing_functions_and_macros_in_doc
 
 
 def check_linking_manpages(pmdk_path, doc_path):
     """
-    Checks if macros and functions from the generated directory have
+    Checks if macros and functions from the doc directory have
     direct access to man page through command 'man function_name/macro_name'
     and returns list with elements which are not linked with the man page.
     """
-    functions_from_generated =\
-        get_functions_and_macros_from_generated(pmdk_path)
+    functions_from_doc = get_functions_and_macros_from_doc(pmdk_path)
     functions_without_manpage = []
-    for function in functions_from_generated:
+    for function in functions_from_doc:
         if doc_path:
             process_find_in_manpage = call(
                 'man -M ' + doc_path + ' ' + function, shell=True,
@@ -231,39 +234,37 @@ def parse_argument(argument_option):
 
 class TestDocumentation(unittest.TestCase):
 
-    def test_completeness_of_macros_in_generated(self):
+    def test_completeness_of_macros_in_doc(self):
         """
-        Checks if all extracted macros from header files are in the generated
+        Checks if all extracted macros from header files are in the doc
         directory.
         """
-        missing_macros =\
-            check_completeness_of_macros_in_generated(pmdk_path)
-        error_msg =\
-            linesep + 'List of missing macros in the generated directory:'
+        missing_macros = check_completeness_of_macros_in_doc(pmdk_path)
+        error_msg = linesep + 'List of missing macros in the doc directory:'
         for macro in missing_macros:
             error_msg += linesep + macro
         self.assertFalse(missing_macros, error_msg)
 
-    def test_completeness_of_functions_in_generated(self):
+    def test_completeness_of_functions_in_doc(self):
         """
-        Checks if all extracted functions from .so files are in the generated
+        Checks if all extracted functions from .so files are in the doc
         directory.
         """
         missing_functions =\
-            check_completeness_of_functions_in_generated(pmdk_path)
-        error_msg =\
-            linesep + 'List of missing functions in the generated directory:'
+            check_completeness_of_functions_in_doc(pmdk_path, lib_path)
+        error_msg = linesep + 'List of missing functions in the doc directory:'
         for function in missing_functions:
             error_msg += linesep + function
         self.assertFalse(missing_functions, error_msg)
 
     def test_completeness_of_extracted_functions_and_macros(self):
         """
-        Checks if all functions and macros from the generated directory,
+        Checks if all functions and macros from the doc directory,
         are extracted from header files and .so files.
         """
         missing_functions_and_macros =\
-            check_completeness_of_extracted_functions_and_macros(pmdk_path)
+            check_completeness_of_extracted_functions_and_macros(
+                pmdk_path, lib_path)
         error_msg = linesep + 'List of missing macros and functions:'
         for elem in missing_functions_and_macros:
             error_msg += linesep + elem
@@ -286,13 +287,25 @@ class TestDocumentation(unittest.TestCase):
 
 if __name__ == '__main__':
     doc_path = ''
+    lib_path = ''
+    with_pmem2_opt = '--with-pmem2'
     if '-h' in sys.argv or '--help' in sys.argv:
         print(__doc__)
         unittest.main()
     elif '-r' in sys.argv:
         pmdk_path = parse_argument('-r')
-        if '-d' in sys.argv:
-            doc_path = parse_argument('-d')
+        if '-i' in sys.argv:
+            installation_path = parse_argument('-i')
+            doc_path = path.join(installation_path, 'share', 'man')
+            lib_path = path.join(installation_path, 'lib')
+        else:
+            print(__doc__)
+            print(unittest.main.__doc__)
+            exit(1)
+        if with_pmem2_opt in sys.argv:
+            PMDK_LIBRARIES.append('libpmem2')
+            index = sys.argv.index(with_pmem2_opt)
+            sys.argv.pop(index)
         unittest.main()
     else:
         print(__doc__)
